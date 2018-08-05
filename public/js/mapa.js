1f68156bfbc78f28fe64feedb4f4090c9dd04f34
Amanda Gomes <amandagomes2010@gmail.com>
2018-08-05 16:15:50 -0300

    infowindow exibindo apenas ultimo registro

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -8,7 +8,6 @@ function initAutocomplete() {
   // Create the search box and link it to the UI element.
   var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP].push(input);
 
   // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', function() {
