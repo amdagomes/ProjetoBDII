a4928a6f0d3c24fae1d3cb8d0eb895d5836c998d
Amanda Gomes <amandagomes2010@gmail.com>
2018-08-04 22:55:57 -0300

    inserção de marcadores no mapa principal

--- app/views/home/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$servidor ="localhost";
-$usuario ="root";
-$senha = "";
-$db_name ="projetobdii";
-$conn= mysqli_connect($servidor,$usuario,$senha,$db_name);
-
- ?>
--- app/views/home/resultado.php
+++ app/views/home/resultado.php
@@ -5,26 +5,20 @@ $usuario ="root";
 $senha = "";
 $db_name ="projetobdii";
 
-/*$conn= mysqli_connect($servidor,$usuario,$senha);
+$conn= mysqli_connect($servidor,$usuario,$senha);
 $banco = mysqli_select_db($conn,$db_name);
 mysqli_set_charset($conn,'utf8');
 
-$sql = mysqli_query($conn,"select * from events") or die("Erro");
+$resultado = mysqli_query($conn,"select * from events") or die("Erro");
 
-
-while($resultado=mysqli_fetch_assoc($sql))
+$eventos = array();
+while($evento = mysqli_fetch_assoc($resultado))
 {
-  $dados[] = array(
-                  "titulo"  =>  $resultado['titulo'],
-                  "tema"    =>  $resultado['tema'],
-                  "periodo" =>  $resultado['periodo'],
-                  "lat"     =>  $resultado['lat'],
-                  "lng"     =>  $resultado['lng']);
-
-  //echo $dados['tema'].'<br>';
+  array_push($eventos,$evento);
 }
-*/
-$con = new mysqli($servidor,$usuario,$senha, $db_name);
+
+
+/*$con = new mysqli($servidor,$usuario,$senha, $db_name);
 if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
 
     //Consultando banco de dados
@@ -36,11 +30,12 @@ if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
         $vetor[] = array_map('utf8_decode', $resultado);
     }
 
-
-    echo json_encode($vetor);
+    $json = json_encode($vetor);
+    echo $json;
 //Passando vetor em forma de json
 
 $fp = fopen("eventos.json", "w");
 fwrite($fp, json_encode($vetor));
 fclose($fp);
+*/
 ?>
