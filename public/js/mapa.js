a7fa86c067fdf726bb71cf27b9279f13ad2d9dd8
Amanda Gomes <amandagomes2010@gmail.com>
2018-08-09 09:58:06 -0300

    permitir adicionar marcador ao clicar

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -1,9 +1,30 @@
+var marker;
+var markers = [];
 function initAutocomplete() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.8688, lng: 151.2195},
-    zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    center: {lat: -6.889582, lng: -38.544935},
+    zoom: 18
   });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Estou aqui!');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 
   // Create the search box and link it to the UI element.
   var input = document.getElementById('pac-input');
@@ -14,7 +35,6 @@ function initAutocomplete() {
     searchBox.setBounds(map.getBounds());
   });
 
-  var markers = [];
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
   searchBox.addListener('places_changed', function() {
@@ -28,6 +48,7 @@ function initAutocomplete() {
     markers.forEach(function(marker) {
       marker.setMap(null);
     });
+
     markers = [];
 
     // For each place, get the icon, name and location.
@@ -41,25 +62,21 @@ function initAutocomplete() {
         scaledSize: new google.maps.Size(25, 25)
       };
 
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
+      //Obter lat long do marcador
+      marker = new google.maps.Marker({
+          map: map,
+          title: place.name,
+          draggable: true,
+          position: place.geometry.location
+      });
+      lat = marker.getPosition().lat();
+      long = marker.getPosition().lng();
+      
+      document.getElementById('lat').value = lat;
+      document.getElementById('long').value = long;
+      markers.push(marker);
 
-                    //Obter lat long do marcador
-                    var marker = new google.maps.Marker({
-                        map: map,
-                        title: place.name,
-                        position: place.geometry.location
-                    });
-                    lat = marker.getPosition().lat();
-                    long = marker.getPosition().lng();
-                    markers.push(marker);
-                    document.getElementById('lat').value = lat;
-                    document.getElementById('long').value = long;
+
       if (place.geometry.viewport) {
         // Only geocodes have viewport.
         bounds.union(place.geometry.viewport);
@@ -67,6 +84,42 @@ function initAutocomplete() {
         bounds.extend(place.geometry.location);
       }
     });
+
     map.fitBounds(bounds);
   });
+
+  //adiciona marcador ao clicar no mapa
+  map.addListener('click', function(e){
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+
+    markers = [];
+
+      //Obter lat long do marcador
+      marker = new google.maps.Marker({
+          map: map,
+          title: document.getElementById('pac-input').value,
+          draggable: true,
+          position: e.latLng
+      });
+      lat = marker.getPosition().lat();
+      long = marker.getPosition().lng();
+
+      document.getElementById('lat').value = lat;
+      document.getElementById('long').value = long;
+      markers.push(marker);
+      console.log("marcador: antes " + marker.getPosition());
+
+      marker.addListener('dragend', function(e) {
+
+        var position =  marker.getPosition();
+        marker.setPosition(position);
+
+      });
+    });
+
+
 }
